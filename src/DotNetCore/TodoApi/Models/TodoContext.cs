0dbd2158cee482f2fcb1c40d1e0d06a718611f9e
aren <aren@what.codes>
2020-09-21 12:19:39 +0800

    Add dotnet core api project

---
+++ src/DotNetCore/TodoApi/Models/TodoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoContext:DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
