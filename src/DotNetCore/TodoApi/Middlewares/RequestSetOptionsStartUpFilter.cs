5f61580282bd7337a2cb58513b286d0b8a748960
aren <aren@what.codes>
2020-09-25 21:43:11 +0800

    add middleware

---
+++ src/DotNetCore/TodoApi/Middlewares/RequestSetOptionsStartUpFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Middlewares
+{
+    public class RequestSetOptionsStartUpFilter : IStartupFilter
+    {
+        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+        {
+            return builder =>
+            {
+                builder.UseMiddleware<RequestSetOptionsMiddleware>();
+                next(builder);
+            };
+        }
+    }
+}
--- src/DotNetCore/TodoApi/Startup.cs
+++ src/DotNetCore/TodoApi/Startup.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using TodoApi.Middlewares;
 using TodoApi.Models;
 
 namespace TodoApi
@@ -32,6 +26,7 @@ namespace TodoApi
             {
                 option.UseInMemoryDatabase("TodoList");
             });
+            services.AddTransient<IStartupFilter, RequestSetOptionsStartUpFilter>();
             services.AddControllers()
                 .AddNewtonsoftJson(setupAction=> {
                     setupAction.UseMemberCasing();
