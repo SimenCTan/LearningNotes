18ea813b94920451b04f5dcf5ce5985e2f337fbc
aren <code@arencore.me>
2020-09-25 21:43:11 +0800

    add middleware

---
+++ src/DotNetCore/TodoApi/Middlewares/RequestSetOptionsMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace TodoApi.Middlewares
+{
+    public class RequestSetOptionsMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public RequestSetOptionsMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext,
+            ILogger<RequestSetOptionsMiddleware> logger)
+        {
+            var option = httpContext.Request.Query["option"];
+            logger.LogInformation("test");
+            if (!string.IsNullOrEmpty(option))
+            {
+                httpContext.Items["option"] = WebUtility.HtmlEncode(option);
+            }
+            await _next(httpContext);
+        }
+    }
+}
