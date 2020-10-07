bfe39d6d0c5a8f94e7d1a0fcf5b52b2a5a73da76
aren <code@arencore.me>
2020-10-07 21:45:35 +0800

    di learn

---
+++ src/DotNetCore/TodoApi/DIServices/IOperation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.DIServices
+{
+    public interface IOperation
+    {
+        string OperationId { get; }
+    }
+
+    public interface IOperationTransien : IOperation
+    {
+    }
+
+    public interface IOperationScope : IOperation
+    {
+    }
+
+    public interface IOperationSingleton : IOperation
+    {
+    }
+}
---
+++ src/DotNetCore/TodoApi/DIServices/Operation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.DIServices
+{
+    public class Operation : IOperationTransien, IOperationScope, IOperationSingleton
+    {
+        public Operation()
+        {
+            OperationId = Guid.NewGuid().ToString()[^4..];
+        }
+        public string OperationId { get; }
+    }
+
+}
