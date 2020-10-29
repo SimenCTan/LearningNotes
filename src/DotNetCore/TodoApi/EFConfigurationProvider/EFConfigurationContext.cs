2c23844f97c77c53073b20706db01fe21ac70cdc
aren <aren@what.codes>
2020-10-29 13:42:24 +0800

    add ef db config

---
+++ src/DotNetCore/TodoApi/EFConfigurationProvider/EFConfigurationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+namespace TodoApi.EFConfigurationProvider
+{
+    public class EFConfigurationContext:DbContext
+    {
+        public EFConfigurationContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<EFConfigurationValue> Values { get; set; }
+    }
+}
---
+++ src/DotNetCore/TodoApi/Extensions/EntityFrameworkExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using TodoApi.EFConfigurationProvider;
+
+namespace TodoApi.Extensions
+{
+    public static class EntityFrameworkExtensions
+    {
+        public static IConfigurationBuilder AddEFConfiguration(this IConfigurationBuilder builder,
+            Action<DbContextOptionsBuilder> optionsAction)
+        {
+            return builder.Add(new EFConfigurationSource(optionsAction));
+        }
+    }
+}
