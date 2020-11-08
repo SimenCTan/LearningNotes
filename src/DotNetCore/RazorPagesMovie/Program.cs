015d8d52d4c387cf382f0c47cb40e50c2b3e1acb
aren <aren@what.codes>
2020-11-08 19:12:55 +0800

    Add Razor page for movies filter

--- src/DotNetCore/RazorPagesMovie/Program.cs
+++ src/DotNetCore/RazorPagesMovie/Program.cs
@@ -1,11 +1,13 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RazorPagesMovie.Pages.Models;
 
 namespace RazorPagesMovie
 {
@@ -13,7 +15,19 @@ namespace RazorPagesMovie
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using var services = host.Services.CreateScope();
+            var serviceProvider = services.ServiceProvider;
+            try
+            {
+                SeedData.Initialize(serviceProvider);
+            }
+            catch (Exception ex)
+            {
+                var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+                logger.LogError($"init error occurred ex {ex.Message}");
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
