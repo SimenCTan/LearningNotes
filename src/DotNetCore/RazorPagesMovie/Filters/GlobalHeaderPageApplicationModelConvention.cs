d9500a875dd87e6857f27d665b0cf1bd287c8752
aren <code@arencore.me>
2020-11-08 23:32:09 +0800

    Add razor page global filter

---
+++ src/DotNetCore/RazorPagesMovie/Filters/GlobalHeaderPageApplicationModelConvention.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie.Filters
+{
+    public class GlobalHeaderPageApplicationModelConvention : IPageApplicationModelConvention
+    {
+        public void Apply(PageApplicationModel model)
+        {
+            model.Filters.Add(new AddHeaderAttribute("Author", "Go"));
+        }
+    }
+}
