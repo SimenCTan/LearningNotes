472539486a8de2c6b0a67205099eea3de1a9de70
aren <code@arencore.me>
2020-11-09 22:03:25 +0800

    mvc add db operation

--- src/DotNetCore/MvcMovies/Startup.cs
+++ src/DotNetCore/MvcMovies/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MvcMovies.Data;
 
 namespace MvcMovies
 {
@@ -24,6 +25,7 @@ namespace MvcMovies
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddMovieDbContextService(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
