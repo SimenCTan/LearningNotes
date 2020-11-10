534d4fe56445542e56673547cca9fea7e9ac96e4
aren <aren@what.codes>
2020-11-10 18:04:30 +0800

    Add xunit test

---
+++ tests/DotNetCoreTest/MvcXUnitTest/HomeControllerTest.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using MvcMovies.Controllers;
+using MvcMovies.Data.Entities;
+using MvcMovies.Data.Repositories;
+using MvcMovies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MvcXUnitTest
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public async Task Index_ReturnsAViewResult_WithAListOfBrainstormSessions()
+        {
+            var mockRepo = new Mock<IBrainstormSessionRepository>();
+            mockRepo.Setup(repo => repo.ListAsync()).ReturnsAsync(GetTestSessions());
+            var homeController = new HomeController(mockRepo.Object);
+
+            // Act
+            var result = homeController.Index();
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<StormSessionViewModel>>(viewResult.ViewData.Model);
+            Assert.Equal(2, model.Count());
+        }
+
+        [Fact]
+        public async Task IndexPost_ReturnsBadRequestResult_WhenModelStateIsInvalid()
+        {
+            var mockRepo = new Mock<IBrainstormSessionRepository>();
+            mockRepo.Setup(repo => repo.ListAsync()).ReturnsAsync(GetTestSessions());
+            var homeController = new HomeController(mockRepo.Object);
+            homeController.ModelState.AddModelError("SessionName", "Required");
+            var seesion = new NewSessionViewModel();
+
+            //Act
+            var result = await homeController.Index(seesion);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.IsType<SerializableError>(badRequestResult.Value);
+        }
+
+        private List<BrainstormSession> GetTestSessions()
+        {
+            var sessions = new List<BrainstormSession>();
+            sessions.Add(new BrainstormSession()
+            {
+                DateCreated = new DateTime(2016, 7, 2),
+                Id = 1,
+                Name = "Test One"
+            });
+            sessions.Add(new BrainstormSession()
+            {
+                DateCreated = new DateTime(2016, 7, 1),
+                Id = 2,
+                Name = "Test Two"
+            });
+            return sessions;
+        }
+    }
+}
