5040fe2aeacc8c6f8abc7e08b13aaa05420f8768
aren <code@arencore.me>
2020-11-10 18:58:04 +0800

    Add test method

---
+++ src/DotNetCore/MvcMovies/Controllers/SessionController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcMovies.Data.Repositories;
+using MvcMovies.Models;
+
+namespace MvcMovies.Controllers
+{
+    public class SessionController : Controller
+    {
+        private readonly IBrainstormSessionRepository _sessionRepository;
+
+        public SessionController(IBrainstormSessionRepository sessionRepository)
+        {
+            _sessionRepository = sessionRepository;
+        }
+
+        public async Task<IActionResult> Index(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return RedirectToAction(actionName: nameof(Index),
+                    controllerName: "Home");
+            }
+
+            var session = await _sessionRepository.GetByIdAsync(id.Value);
+            if (session == null)
+            {
+                return Content("Session not found.");
+            }
+
+            var viewModel = new StormSessionViewModel()
+            {
+                DateCreated = session.DateCreated,
+                Name = session.Name,
+                Id = session.Id
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ tests/DotNetCoreTest/MvcXUnitTest/IdeaControllerTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using MvcMovies.Api;
+using MvcMovies.Data.Entities;
+using MvcMovies.Data.Repositories;
+using MvcMovies.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MvcXUnitTest
+{
+    public class IdeaControllerTest
+    {
+        [Fact]
+        public async Task ForSession_ReturnsHttpNotFound_ForInvalidSession()
+        {
+            // Arrange
+            int testSessionId = 123;
+            var mockRepo = new Mock<IBrainstormSessionRepository>();
+            mockRepo.Setup(repo => repo.GetByIdAsync(testSessionId))
+                .ReturnsAsync((BrainstormSession)null);
+            var controller = new IdeasController(mockRepo.Object);
+
+            // Act
+            var result = await controller.ForSession(testSessionId);
+
+            // Assert
+            var notFoundObjectResult = Assert.IsType<NotFoundObjectResult>(result);
+            Assert.Equal(testSessionId, notFoundObjectResult.Value);
+        }
+    }
+}
