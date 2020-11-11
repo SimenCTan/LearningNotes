a7762600b3e9cfb3df3c481c8823eaa91d2b03ab
aren <aren@what.codes>
2020-11-11 22:33:03 +0800

    Add session save value

--- src/DotNetCore/RazorPagesMovie/Pages/Index.cshtml.cs
+++ src/DotNetCore/RazorPagesMovie/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,6 +12,9 @@ namespace RazorPagesMovie.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public const string SessionKeyName = "_Name";
+        public const string SessionKeyAge = "_Age";
+        const string SessionKeyTime = "_Time";
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -19,7 +23,15 @@ namespace RazorPagesMovie.Pages
 
         public void OnGet()
         {
+            // Requires: using Microsoft.AspNetCore.Http;
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString(SessionKeyName)))
+            {
+                HttpContext.Session.SetString(SessionKeyName, "The Doctor");
+                HttpContext.Session.SetInt32(SessionKeyAge, 773);
+            }
 
+            var name = HttpContext.Session.GetString(SessionKeyName);
+            var age = HttpContext.Session.GetInt32(SessionKeyAge);
         }
     }
 }
