a070475da18ce0f2af0dd0cf7152f6d4e6e611cc
aren <code@arencore.me>
2020-11-18 23:57:26 +0800

    upload file learn note

---
+++ src/DotNetCore/UploadFile/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UploadFile.Models;
+
+namespace UploadFile.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<AppFile> AppFiles { get; set; }
+    }
+}
