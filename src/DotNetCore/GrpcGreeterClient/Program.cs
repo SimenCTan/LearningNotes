1dc84eb24bd90eb13bc183e3c8af91d83cf88a88
aren <aren@what.codes>
2020-11-25 23:54:59 +0800

    :sparkles: add grpc server and client

---
+++ src/DotNetCore/GrpcGreeterClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "GrpcGreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
--- src/DotNetCore/TodoApi/Middlewares/RequestSetOptionsStartUpFilter.cs
+++ src/DotNetCore/TodoApi/Middlewares/RequestSetOptionsStartUpFilter.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace TodoApi.Middlewares
+
 {
     public class RequestSetOptionsStartUpFilter : IStartupFilter
     {
