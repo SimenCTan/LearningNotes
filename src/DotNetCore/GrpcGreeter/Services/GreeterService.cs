56db5f6e6a3e3f9454fc62378a97682e896f7f79
aren <aren@what.codes>
2020-12-06 19:03:08 +0800

    :sparkles: Add grpc stream request and response

--- src/DotNetCore/GrpcGreeter/Services/GreeterService.cs
+++ src/DotNetCore/GrpcGreeter/Services/GreeterService.cs
@@ -1,8 +1,6 @@
-using Grpc.Core;
+﻿using Grpc.Core;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace GrpcGreeter
@@ -21,6 +19,50 @@ namespace GrpcGreeter
             {
                 Message = "Hello " + request.Name
             });
+        }
+
+        public override async Task StreamingFromService(ExampleRequest request, IServerStreamWriter<ExampleResponse> responseStream, ServerCallContext context)
+        {
+            while (!context.CancellationToken.IsCancellationRequested)
+            {
+                await responseStream.WriteAsync(new ExampleResponse { Age = new Random().Next(1,100) });
+                await Task.Delay(TimeSpan.FromSeconds(1), context.CancellationToken);
+            }
+        }
+
+        public override async Task<ExampleResponse> StreamingFromClient(IAsyncStreamReader<ExampleRequest> requestStream, ServerCallContext context)
+        {
+            while (await requestStream.MoveNext())
+            {
+                var message = requestStream.Current;
+                Console.WriteLine($"request message pageindex: {message.PageIndex},pagesize: {message.PageSize},order {message.IsDescending}");
+            }
+            return new ExampleResponse { Age = 99 };
+        }
+
+        public override async Task StreamingBothWays(IAsyncStreamReader<ExampleRequest> requestStream, IServerStreamWriter<ExampleResponse> responseStream, ServerCallContext context)
+        {
+            await foreach (var message in requestStream.ReadAllAsync())
+            {
+                Console.WriteLine($"request message pageindex: {message.PageIndex},pagesize: {message.PageSize},order {message.IsDescending}");
+                await responseStream.WriteAsync(new ExampleResponse { Age = new Random().Next(1, 100) });
+            }
+
+            // Read requests in a background task.
+            //var readTask = Task.Run(async () =>
+            //{
+            //    await foreach (var message in requestStream.ReadAllAsync())
+            //    {
+            //        // Process request.
+            //    }
+            //});
+
+            //// Send responses until the client signals that it is complete.
+            //while (!readTask.IsCompleted)
+            //{
+            //    await responseStream.WriteAsync(new ExampleResponse());
+            //    await Task.Delay(TimeSpan.FromSeconds(1), context.CancellationToken);
+            //}
         }
     }
 }
