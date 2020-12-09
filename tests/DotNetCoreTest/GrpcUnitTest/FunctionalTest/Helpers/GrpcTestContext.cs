9d1a7833de13eb5e822a20492d84b290f26a7668
aren <code@arencore.me>
2020-12-09 13:17:13 +0800

    :sparkles: Add Grpc integration test

---
+++ tests/DotNetCoreTest/GrpcUnitTest/FunctionalTest/Helpers/GrpcTestContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcUnitTest.FunctionalTest.Helpers
+{
+    internal class GrpcTestContext<TStartup> : IDisposable where TStartup : class
+    {
+        private readonly ExecutionContext _executionContext;
+        private readonly Stopwatch _stopwatch;
+        private readonly GrpcTestFixture<TStartup> _fixture;
+        public GrpcTestContext(GrpcTestFixture<TStartup> fixture)
+        {
+            _executionContext = ExecutionContext.Capture()!;
+            _stopwatch = Stopwatch.StartNew();
+            _fixture = fixture;
+        }
+
+        private void WriteMessage(LogLevel logLevel, string category, EventId eventId, string message, Exception exception)
+        {
+            // Log using the passed in execution context.
+            // In the case of NUnit, console output is only captured by the test
+            // if it is written in the test's execution context.
+            ExecutionContext.Run(_executionContext, s =>
+            {
+                Console.WriteLine($"{_stopwatch.Elapsed.TotalSeconds:N3}s {category} - {logLevel}: {message}");
+            }, null);
+        }
+        public void Dispose()
+        {
+            _fixture.LoggedMessage -= WriteMessage;
+            _executionContext?.Dispose();
+        }
+    }
+}
---
+++ tests/DotNetCoreTest/GrpcUnitTest/FunctionalTest/Helpers/GrpcTestFixture.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using System.Net.Http;
+
+namespace GrpcUnitTest.FunctionalTest.Helpers
+{
+    public delegate void LogMessage(LogLevel logLevel, string categoryName, EventId eventId, string message, Exception exception);
+    public class GrpcTestFixture<TStartup> : IDisposable where TStartup : class
+    {
+        private readonly TestServer _server;
+        private readonly IHost _host;
+        public event LogMessage? LoggedMessage;
+        public HttpMessageHandler Handler { get; }
+        public LoggerFactory LoggerFactory { get; }
+
+        public GrpcTestFixture() : this(null) { }
+
+        public GrpcTestFixture(Action<IServiceCollection>? initialConfigureServices)
+        {
+            LoggerFactory = new LoggerFactory();
+            LoggerFactory.AddProvider(new ForwardingLoggerProvider((logLevel, category, eventId, message, exception) =>
+            {
+                LoggedMessage?.Invoke(logLevel, category, eventId, message, exception);
+            }));
+            var builder = new HostBuilder()
+                            .ConfigureServices(service =>
+                            {
+                                initialConfigureServices?.Invoke(service);
+                                service.AddSingleton<ILoggerFactory>(LoggerFactory);
+                            })
+                            .ConfigureWebHostDefaults(webhost =>
+                            {
+                                webhost.UseTestServer()
+                                .UseStartup<TStartup>();
+                            });
+            _host = builder.Start();
+            _server = _host.GetTestServer();
+            Handler = _server.CreateHandler();
+        }
+        public void Dispose()
+        {
+            Handler.Dispose();
+            _host.Dispose();
+            _server.Dispose();
+        }
+
+        public IDisposable GetTestContext()
+        {
+            return new GrpcTestContext<TStartup>(this);
+        }
+    }
+}
