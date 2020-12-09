c83d0ff1f11fb5eb39fa9870eb8fdaf695432c6f
aren <aren@what.codes>
2020-12-09 13:17:13 +0800

    :sparkles: Add Grpc integration test

---
+++ tests/DotNetCoreTest/GrpcUnitTest/FunctionalTest/FunctionalTestBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Grpc.Net.Client;
+using GrpcUnitTest.FunctionalTest.Helpers;
+using GrpcGreeter;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace GrpcUnitTest.FunctionalTest
+{
+    public class FunctionalTestBase
+    {
+        private GrpcChannel? _channel;
+        private IDisposable? _testContext;
+        protected GrpcTestFixture<Startup> Fixture { get; private set; } = default!;
+        protected ILoggerFactory LoggerFactory => Fixture.LoggerFactory;
+        protected GrpcChannel Channel => _channel ??= CreateGrpcChannel();
+        protected GrpcChannel CreateGrpcChannel()
+        {
+            return GrpcChannel.ForAddress("http://localhost", new GrpcChannelOptions
+            {
+                LoggerFactory = LoggerFactory,
+                HttpHandler=Fixture.Handler,
+                ThrowOperationCanceledOnCancellation=true
+            });
+        }
+        protected virtual void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            Fixture = new GrpcTestFixture<Startup>(ConfigureServices);
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            Fixture.Dispose();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _testContext = Fixture.GetTestContext();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _testContext?.Dispose();
+            _channel = null;
+        }
+    }
+}
---
+++ tests/DotNetCoreTest/GrpcUnitTest/FunctionalTest/GreeterServiceTests.cs
@@ -0,0 +1,98 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using GrpcGreeter;
+using System.Threading;
+using Grpc.Core;
+
+namespace GrpcUnitTest.FunctionalTest
+{
+    [TestFixture]
+    public class GreeterServiceTests:FunctionalTestBase
+    {
+        [Test]
+        public async Task SayHelloTest()
+        {
+            // init
+            var client = new Greeter.GreeterClient(Channel);
+
+            // act
+            var response = await client.SayHelloAsync(new HelloRequest { Name = "test" });
+
+            // assert
+            Assert.AreEqual("Hello test", response.Message);
+        }
+
+        [Test]
+        public async Task StreamingFromServiceTest()
+        {
+            //arrange
+            var client = new Greeter.GreeterClient(Channel);
+            var cts = new CancellationTokenSource();
+            var hasMessage = false;
+            var callCancelled = false;
+
+            // act
+            using var call = client.StreamingFromService(new ExampleRequest {PageIndex=1,PageSize=10,IsDescending=false },cancellationToken:cts.Token);
+            try
+            {
+                await foreach (var message in call.ResponseStream.ReadAllAsync())
+                {
+                    hasMessage = true;
+                    cts.Cancel();
+                }
+            }
+            catch (OperationCanceledException ex)
+            {
+                callCancelled = true;
+            }
+
+            // assert
+            Assert.IsTrue(callCancelled);
+            Assert.IsTrue(hasMessage);
+        }
+
+        [Test]
+        public async Task StreamingFromClientTest()
+        {
+            // arrange
+            var client = new Greeter.GreeterClient(Channel);
+            var indexs = new[] { 1, 2,3 };
+            ExampleResponse exampleResponse;
+
+            // act
+            using var call = client.StreamingFromClient();
+            foreach (var index in indexs)
+            {
+                await call.RequestStream.WriteAsync(new ExampleRequest { PageIndex = index, PageSize = 10, IsDescending = false });
+            }
+            await call.RequestStream.CompleteAsync();
+            exampleResponse = await call;
+            Assert.IsTrue(exampleResponse.Age > 1);
+        }
+
+        [Test]
+        public async Task StreamingBothWaysTest()
+        {
+            // arrange
+            var client = new Greeter.GreeterClient(Channel);
+            var indexs = new[] { 1, 2, 3 };
+            var ages = new List<int?>();
+
+            // act
+            using var call = client.StreamingBothWays();
+            foreach (var index in indexs)
+            {
+                await call.RequestStream.WriteAsync(new ExampleRequest { PageIndex = index, PageSize = 10, IsDescending = false });
+                Assert.IsTrue(await call.ResponseStream.MoveNext());
+                ages.Add(call.ResponseStream.Current.Age);
+            }
+            await call.RequestStream.CompleteAsync();
+
+            //assert
+            Assert.AreEqual(ages.Count, 3);
+        }
+    }
+}
