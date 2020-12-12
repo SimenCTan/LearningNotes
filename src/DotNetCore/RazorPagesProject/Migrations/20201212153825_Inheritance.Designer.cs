1442cc1f59b064e044a8a1fffb8dde9cf79089a9
aren <aren@what.codes>
2020-12-12 23:52:54 +0800

    :lipstick: Add ef core inheritance

--- src/DotNetCore/RazorPagesProject/Migrations/20201212153825_Inheritance.Designer.cs
+++ src/DotNetCore/RazorPagesProject/Migrations/20201212153825_Inheritance.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesProject.Data;
 
 namespace RazorPagesProject.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201212153825_Inheritance")]
+    partial class Inheritance
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- tests/DotNetCoreTest/RazorPagesProject.Tests/Helpers/Utilities.cs
+++ tests/DotNetCoreTest/RazorPagesProject.Tests/Helpers/Utilities.cs
@@ -11,6 +11,8 @@ namespace RazorPagesProject.Tests.Helpers
         public static void InitializeDbForTests(AppDbContext db)
         {
             db.Messages.AddRange(GetSeedingMessages());
+            db.Students.AddRange(GetStudents());
+            db.Instructors.AddRange(GetInstructors());
             db.SaveChanges();
         }
 
@@ -28,6 +30,26 @@ namespace RazorPagesProject.Tests.Helpers
                 new Message(){ Text = "TEST RECORD: Would you like a jelly baby?" },
                 new Message(){ Text = "TEST RECORD: To the rational mind, " +
                     "nothing is inexplicable; only unexplained." }
+            };
+        }
+
+        private static List<Student> GetStudents()
+        {
+            return new List<Student>()
+            {
+                new Student{ FirstMidName="test",LastName="student",EnrollmentDate=DateTime.Now},
+                new Student{ FirstMidName="test1",LastName="student1",EnrollmentDate=DateTime.Now},
+                new Student{ FirstMidName="test2",LastName="student2",EnrollmentDate=DateTime.Now}
+            };
+        }
+
+        private static List<Instructor> GetInstructors()
+        {
+            return new List<Instructor>()
+            {
+                new Instructor{ FirstMidName="test",LastName="Instructor",HireDate=DateTime.Now},
+                new Instructor{ FirstMidName="test1",LastName="Instructor1",HireDate=DateTime.Now},
+                new Instructor{ FirstMidName="test2",LastName="Instructor2",HireDate=DateTime.Now}
             };
         }
     }
