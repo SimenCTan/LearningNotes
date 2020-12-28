c2f9e14f386b43150b0d371e42fb2a1b8d2f5985
aren <code@arencore.me>
2020-12-28 23:45:20 +0800

    :sparkles: Add health check

---
+++ src/DotNetCore/HealthChecksSample/BasicStartup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HealthChecksSample
+{
+    public class BasicStartup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHealthChecks();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHealthChecks("/health");
+            });
+        }
+    }
+}
