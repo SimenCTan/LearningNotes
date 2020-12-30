69742ebb18ad83308dea4bef38b8d3b5893e00d7
aren <code@arencore.me>
2020-12-30 23:22:16 +0800

    :recycle: Add razor page role policy authorize

--- src/DotNetCore/Identity/Pages/Index.cshtml.cs
+++ src/DotNetCore/Identity/Pages/Index.cshtml.cs
@@ -5,9 +5,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Identity.Pages
 {
+    [AllowAnonymous]
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
