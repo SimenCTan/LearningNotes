bd48b08800969c7262c4b0b82f35c157a3c90be4
aren <aren@what.codes>
2021-04-20 23:42:14 +0800

    :lipstick:

---
+++ src/dapr/pythonapp/app1.py
@@ -0,0 +1,23 @@
+import flask
+from flask import request, jsonify
+from flask_cors import CORS
+import json
+import sys
+
+app = flask.Flask(__name__)
+CORS(app)
+
+@app.route('/dapr/subscribe', methods=['GET'])
+def subscribe():
+    subscriptions = [{'pubsubname': 'pubsub',
+                      'topic': 'deathStarStatus',
+                      'route': 'dsstatus'}]
+    return jsonify(subscriptions)
+
+@app.route('/dsstatus', methods=['POST'])
+def ds_subscriber():
+    print(request.json, flush=True)
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+app.run()
+
+
