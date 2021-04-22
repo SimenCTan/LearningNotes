8131f5ab4747c8c00ae8301607ae513d38ac35b4
aren <aren@what.codes>
2021-04-23 00:09:15 +0800

    :sparkles: finish green browser

--- src/JavaScript/WebDevForBegin/GreenBrowser/src/index.js
+++ src/JavaScript/WebDevForBegin/GreenBrowser/src/index.js
@@ -1,4 +1,4 @@
-import axios from "./node_modules/axios";
+import axios from "axios";
 
 // form fields
 const form = document.querySelector(".form-data");
@@ -13,97 +13,6 @@ const usage = document.querySelector(".carbon-usage");
 const fossilfuel = document.querySelector(".fossil-fuel");
 const myregion = document.querySelector(".my-region");
 const clearBtn = document.querySelector(".clear-btn");
-
-form.addEventListener("submit", (e) => handlerSubmit(e));
-clearBtn.addEventListener("click", (e) => reset(e));
-init();
-
-function handlerSubmit() {}
-
-function init() {
-  //if anything is in localStorage, pick it up
-  const storeApiKey = localStorage.getItem("apiKey");
-  const storeRegion = localStorage.getItem("regionName");
-
-  //set icon to be generic green
-  chrome.runtime.sendMessage({
-    action: "updateIcon",
-    value: {
-      color: "green",
-    },
-  });
-  if (storeApiKey === null || storeRegion === null) {
-    //if we don't have the keys, show the form
-    form.style.display = "block";
-    results.style.display = "none";
-    loading.style.display = "none";
-    clearBtn.style.display = "none";
-    errors.textContent = "";
-  } else {
-    //if we have saved keys/regions in localStorage, show results when they load
-    displayCarbonUsage(storedApiKey, storedRegion);
-    results.style.display = "none";
-    form.style.display = "none";
-    clearBtn.style.display = "block";
-  }
-}
-
-function reset() {
-  e.preventDefault();
-  //clear local storage for region only
-  localStorage.removeItem("regionName");
-  init();
-}
-
-function handlerSubmit(e) {
-  e.preventDefault();
-  setUpUser(apiKey.Value, region.Value);
-}
-
-function setUpUser(apiKey, regionName) {
-  localStorage.setItem("apiKey", apiKey);
-  localStorage.setItem("regionName", regionName);
-  loading.style.display = "block";
-  errors.textContent = "";
-  clearBtn.style.display = "block";
-  //make initial call
-  displayCarbonUsage(apiKey, regionName);
-}
-
-async function displayCarbonUsage(apiKey, regionName) {
-  try {
-    await axios
-      .get("https://api.co2signal.com/v1/latest", {
-        params: {
-          countryCode: region,
-        },
-        headers: {
-          "auth-token": apiKey,
-        },
-      })
-      .then((response) => {
-        let CO2 = Math.floor(response.data.data.carbonIntensity);
-
-        calculateColor(CO2);
-        loading.style.display = "none";
-        form.style.display = "none";
-        myregion.textContent = region;
-        usage.textContent =
-          Math.round(response.data.data.carbonIntensity) +
-          " grams (grams C02 emitted per kilowatt hour)";
-        fossilfuel.textContent =
-          response.data.data.fossilFuelPercentage.toFixed(2) +
-          "% (percentage of fossil fuels used to generate electricity)";
-        results.style.display = "block";
-      });
-  } catch (error) {
-    console.log(error);
-    loading.style.display = "none";
-    results.style.display = "none";
-    errors.textContent =
-      "Sorry, we have no data for the region you have requested.";
-  }
-}
 
 calculateColor = async (value) => {
   let co2Scale = [0, 150, 600, 750, 800];
@@ -124,3 +33,100 @@ calculateColor = async (value) => {
     value: { color: closestColor },
   });
 };
+
+const displayCarbonUsage = async (apiKey, region) => {
+  try {
+    await axios
+      .get("https://api.co2signal.com/v1/latest", {
+        params: {
+          countryCode: region,
+        },
+        headers: {
+          //please get your own token from CO2Signal https://www.co2signal.com/
+          "auth-token": apiKey,
+        },
+      })
+      .then((response) => {
+        let CO2 = Math.floor(response.data.data.carbonIntensity);
+
+        calculateColor(CO2);
+
+        loading.style.display = "none";
+        form.style.display = "none";
+        myregion.textContent = region;
+        usage.textContent =
+          Math.round(response.data.data.carbonIntensity) +
+          " grams (grams C02 emitted per kilowatt hour)";
+        fossilfuel.textContent =
+          response.data.data.fossilFuelPercentage.toFixed(2) +
+          "% (percentage of fossil fuels used to generate electricity)";
+        results.style.display = "block";
+      });
+  } catch (error) {
+    console.log(error);
+    loading.style.display = "none";
+    results.style.display = "none";
+    errors.textContent =
+      "Sorry, we have no data for the region you have requested.";
+  }
+};
+
+// set up api key and region
+const setUpUser = async (apiKey, regionName) => {
+  localStorage.setItem("apiKey", apiKey);
+  localStorage.setItem("regionName", regionName);
+  loading.style.display = "block";
+  errors.textContent = "";
+  clearBtn.style.display = "block";
+  //make initial call
+  displayCarbonUsage(apiKey, regionName);
+};
+
+// handle form submission
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  setUpUser(apiKey.value, region.value);
+};
+
+//initial checks
+const init = async () => {
+  //if anything is in localStorage, pick it up
+  const storedApiKey = localStorage.getItem("apiKey");
+  const storedRegion = localStorage.getItem("regionName");
+
+  //set icon to be generic green
+  chrome.runtime.sendMessage({
+    action: "updateIcon",
+    value: {
+      color: "green",
+    },
+  });
+
+  if (storedApiKey === null || storedRegion === null) {
+    //if we don't have the keys, show the form
+    form.style.display = "block";
+    results.style.display = "none";
+    loading.style.display = "none";
+    clearBtn.style.display = "none";
+    errors.textContent = "";
+  } else {
+    //if we have saved keys/regions in localStorage, show results when they load
+    results.style.display = "none";
+    form.style.display = "none";
+    displayCarbonUsage(storedApiKey, storedRegion);
+    clearBtn.style.display = "block";
+  }
+};
+
+const reset = async (e) => {
+  e.preventDefault();
+  //clear local storage for region only
+  localStorage.removeItem("regionName");
+  init();
+};
+
+form.addEventListener("submit", (e) => handleSubmit(e));
+clearBtn.addEventListener("click", (e) => reset(e));
+
+//start app
+init();
