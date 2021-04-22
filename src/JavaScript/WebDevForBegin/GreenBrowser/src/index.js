ec85bff233a552e7f1f9decdb4ccc065a49c40d5
aren <code@arencore.me>
2021-04-22 10:57:16 +0800

    :lipstick: add index.js

---
+++ src/JavaScript/WebDevForBegin/GreenBrowser/src/index.js
@@ -0,0 +1,126 @@
+import axios from "./node_modules/axios";
+
+// form fields
+const form = document.querySelector(".form-data");
+const region = document.querySelector(".region-name");
+const apiKey = document.querySelector(".api-key");
+
+// results
+const errors = document.querySelector(".errors");
+const loading = document.querySelector(".loading");
+const results = document.querySelector(".result-container");
+const usage = document.querySelector(".carbon-usage");
+const fossilfuel = document.querySelector(".fossil-fuel");
+const myregion = document.querySelector(".my-region");
+const clearBtn = document.querySelector(".clear-btn");
+
+form.addEventListener("submit", (e) => handlerSubmit(e));
+clearBtn.addEventListener("click", (e) => reset(e));
+init();
+
+function handlerSubmit() {}
+
+function init() {
+  //if anything is in localStorage, pick it up
+  const storeApiKey = localStorage.getItem("apiKey");
+  const storeRegion = localStorage.getItem("regionName");
+
+  //set icon to be generic green
+  chrome.runtime.sendMessage({
+    action: "updateIcon",
+    value: {
+      color: "green",
+    },
+  });
+  if (storeApiKey === null || storeRegion === null) {
+    //if we don't have the keys, show the form
+    form.style.display = "block";
+    results.style.display = "none";
+    loading.style.display = "none";
+    clearBtn.style.display = "none";
+    errors.textContent = "";
+  } else {
+    //if we have saved keys/regions in localStorage, show results when they load
+    displayCarbonUsage(storedApiKey, storedRegion);
+    results.style.display = "none";
+    form.style.display = "none";
+    clearBtn.style.display = "block";
+  }
+}
+
+function reset() {
+  e.preventDefault();
+  //clear local storage for region only
+  localStorage.removeItem("regionName");
+  init();
+}
+
+function handlerSubmit(e) {
+  e.preventDefault();
+  setUpUser(apiKey.Value, region.Value);
+}
+
+function setUpUser(apiKey, regionName) {
+  localStorage.setItem("apiKey", apiKey);
+  localStorage.setItem("regionName", regionName);
+  loading.style.display = "block";
+  errors.textContent = "";
+  clearBtn.style.display = "block";
+  //make initial call
+  displayCarbonUsage(apiKey, regionName);
+}
+
+async function displayCarbonUsage(apiKey, regionName) {
+  try {
+    await axios
+      .get("https://api.co2signal.com/v1/latest", {
+        params: {
+          countryCode: region,
+        },
+        headers: {
+          "auth-token": apiKey,
+        },
+      })
+      .then((response) => {
+        let CO2 = Math.floor(response.data.data.carbonIntensity);
+
+        calculateColor(CO2);
+        loading.style.display = "none";
+        form.style.display = "none";
+        myregion.textContent = region;
+        usage.textContent =
+          Math.round(response.data.data.carbonIntensity) +
+          " grams (grams C02 emitted per kilowatt hour)";
+        fossilfuel.textContent =
+          response.data.data.fossilFuelPercentage.toFixed(2) +
+          "% (percentage of fossil fuels used to generate electricity)";
+        results.style.display = "block";
+      });
+  } catch (error) {
+    console.log(error);
+    loading.style.display = "none";
+    results.style.display = "none";
+    errors.textContent =
+      "Sorry, we have no data for the region you have requested.";
+  }
+}
+
+calculateColor = async (value) => {
+  let co2Scale = [0, 150, 600, 750, 800];
+  let colors = ["#2AA364", "#F5EB4D", "#9E4229", "#381D02", "#381D02"];
+
+  let closestNum = co2Scale.sort((a, b) => {
+    return Math.abs(a - value) - Math.abs(b - value);
+  })[0];
+  //console.log(value + ' is closest to ' + closestNum);
+  let num = (element) => element > closestNum;
+  let scaleIndex = co2Scale.findIndex(num);
+
+  let closestColor = colors[scaleIndex];
+  //console.log(scaleIndex, closestColor);
+
+  chrome.runtime.sendMessage({
+    action: "updateIcon",
+    value: { color: closestColor },
+  });
+};
