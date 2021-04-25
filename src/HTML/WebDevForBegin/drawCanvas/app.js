f1db0edf7f00b36f075bcc51eec9550a478a32b5
aren <code@arencore.me>
2021-04-25 23:36:09 +0800

    :lipsitck: finish draw canvas

--- src/HTML/WebDevForBegin/drawCanvas/app.js
+++ src/HTML/WebDevForBegin/drawCanvas/app.js
@@ -1,19 +1,19 @@
 function loadTexture(path) {
   return new Promise((resolve) => {
     const img = new Image();
-    import.src = path;
+    img.src = path;
     img.onload = () => {
       resolve(img);
     };
   });
 }
 
-function CreateEnemies(ctx, canvas, enemyImg) {
+function createEnemies(ctx, canvas, enemyImg) {
   const MONSTER_TOTAL = 5;
   const MONSTER_WIDTH = MONSTER_TOTAL * 98;
   const START_X = (canvas.width - MONSTER_WIDTH) / 2;
   const STOP_X = START_X + MONSTER_WIDTH;
-  for (let x = START_X; x < STOP_X; x + 98) {
+  for (let x = START_X; x < STOP_X; x += 98) {
     for (let y = 0; y < 50 * MONSTER_TOTAL; y += 50) {
       ctx.drawImage(enemyImg, x, y);
     }
@@ -23,8 +23,8 @@ function CreateEnemies(ctx, canvas, enemyImg) {
 window.onload = async () => {
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
-  const heroImage = loadTexture("assets/player.png");
-  const enemyImg = loadTexture("assets/enemyShip.png");
+  const heroImage = await loadTexture("assets/player.png");
+  const enemyImg = await loadTexture("assets/enemyShip.png");
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(
