0c2daf678267ea85f464154039a7512461a32c6d
aren <aren@what.codes>
2021-04-25 23:27:34 +0800

    draw canvas

---
+++ src/HTML/WebDevForBegin/drawCanvas/app.js
@@ -0,0 +1,36 @@
+function loadTexture(path) {
+  return new Promise((resolve) => {
+    const img = new Image();
+    import.src = path;
+    img.onload = () => {
+      resolve(img);
+    };
+  });
+}
+
+function CreateEnemies(ctx, canvas, enemyImg) {
+  const MONSTER_TOTAL = 5;
+  const MONSTER_WIDTH = MONSTER_TOTAL * 98;
+  const START_X = (canvas.width - MONSTER_WIDTH) / 2;
+  const STOP_X = START_X + MONSTER_WIDTH;
+  for (let x = START_X; x < STOP_X; x + 98) {
+    for (let y = 0; y < 50 * MONSTER_TOTAL; y += 50) {
+      ctx.drawImage(enemyImg, x, y);
+    }
+  }
+}
+
+window.onload = async () => {
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+  const heroImage = loadTexture("assets/player.png");
+  const enemyImg = loadTexture("assets/enemyShip.png");
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(
+    heroImage,
+    canvas.width / 2 - 45,
+    canvas.height - canvas.height / 4
+  );
+  createEnemies(ctx, canvas, enemyImg);
+};
