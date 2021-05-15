f12ca45b342be095d1b428149184bfbf83eb78ce
aren <aren@what.codes>
2021-05-15 23:13:18 +0800

    :lipstick: Add laser deal with collision

--- src/JavaScript/WebDevForBegin/SpaceGame/movingelement/app.js
+++ src/JavaScript/WebDevForBegin/SpaceGame/movingelement/app.js
@@ -91,3 +91,55 @@ window.addEventListener("keyup", (evt) => {
     eventEmitter.emit(Messages.KEY_EVENT_RIGHT);
   }
 });
+
+window.onload()=async()=>{
+  canvas=document.getElementById("canvas");
+  ctx=canvas.getContext("2d");
+  heroImg = await loadTexture('assets/player.png');
+	enemyImg = await loadTexture('assets/enemyShip.png');
+	laserImg = await loadTexture('assets/laserRed.png');
+  initGame();
+  let gameLoopId=setInterval(()=>{
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    drawGameObjects(ctx);
+  },100);
+}
+
+function loadTexture(path){
+  return new Promise((resolve)=>{
+    const img=new Image();
+    img.src=path;
+    img.onload=()=>{
+      resolve(img);
+    };
+  });
+}
+
+function drawGameObjects(ctx){
+  gameObjects.forEach((go)=>go.draw(ctx));
+}
+
+function  createEnemies(){
+  const MONSTER_TOTAL=5;
+  const MONSTER_WIDTH=MONSTER_TOTAL*98;
+  const START_X=(canvas.width-MONSTER_WIDTH)/2;
+  const STOP_X=START_X+MONSTER_WIDTH;
+
+  for(let x=START_X;x<STOP_X;x+=98){
+    for(let y=0;y<50*5;y+=50){
+      const enemy=new Enemy();
+      gameObjects.push(enemy);
+    }
+  }
+}
+
+function createHero() {
+  hero = new Hero(
+    canvas.width / 2 - 45,
+    canvas.height - canvas.height / 4
+  );
+  hero.img = heroImg;
+  gameObjects.push(hero);
+}
