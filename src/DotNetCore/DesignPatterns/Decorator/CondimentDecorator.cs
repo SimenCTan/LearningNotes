e7e931a4617dbce11009d5a4347fb0106ae697e0
aren <aren@what.codes>
2021-05-26 15:40:58 +0800

    :lipstick: base constructor

--- src/DotNetCore/DesignPatterns/Decorator/CondimentDecorator.cs
+++ src/DotNetCore/DesignPatterns/Decorator/CondimentDecorator.cs
@@ -2,5 +2,10 @@ namespace DesignPatterns.Decorator
 {
     public abstract class CondimentDecorator:BeverageBase
     {
+        protected readonly BeverageBase _beverage;
+        public CondimentDecorator(BeverageBase beverage)
+        {
+            _beverage=beverage;
+        }
     }
 }
