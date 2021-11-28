85327a3673d02b5d97194da182a5643dcf16699a
aren <code@arencore.me>
2021-11-28 23:47:58 +0800

    update signal hub

--- src/DotNetCore/SignalRChat/Hubs/ChatHub.cs
+++ src/DotNetCore/SignalRChat/Hubs/ChatHub.cs
@@ -3,11 +3,22 @@ using System.Threading.Tasks;
 
 namespace SignalRChat.Hubs
 {
-    public class ChatHub : Hub
+    public class StronglyTypedChatHub : Hub<IChatClient>
     {
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.ReceiveMessage(user, message);
+        }
+
+        public Task SendMessageToCaller(string user, string message)
+        {
+            return Clients.Caller.ReceiveMessage(user, message);
+        }
+
+        [HubMethodName("SendMessageToUser")]
+        public Task DirectMessage(string user, string message)
+        {
+            return Clients.User(user).ReceiveMessage(user, message);
         }
     }
 }
---
+++ src/DotNetCore/SignalRChat/Hubs/IChatClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Hubs
+{
+    public interface IChatClient
+    {
+        Task ReceiveMessage(string user, string message);
+    }
+}
