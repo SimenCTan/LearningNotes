491f8008fe53cbff1f53ea2c185e73c4a5c502b0
aren <code@arencore.me>
2021-11-29 18:19:08 +0800

    update

---
+++ src/DotNetCore/BenchmarkDemo/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Running;
+
+Console.WriteLine("Hello, World!");
+var summary = BenchmarkRunner.Run<Md5VsSha256>();
