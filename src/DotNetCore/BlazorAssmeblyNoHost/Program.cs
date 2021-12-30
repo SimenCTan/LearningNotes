558972e9c6e60d318b30c5d32a044140a8483bb2
aren <code@arencore.me>
2021-12-30 15:44:59 +0800

    Add other config

--- src/DotNetCore/BlazorAssmeblyNoHost/Program.cs
+++ src/DotNetCore/BlazorAssmeblyNoHost/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorAssmeblyNoHost;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+var http = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
+builder.Services.AddScoped(sp => http);
+using var response = await http.GetAsync("car.json");
+using var stream = await response.Content.ReadAsStreamAsync();
+builder.Configuration.AddJsonStream(stream);
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 await builder.Build().RunAsync();
