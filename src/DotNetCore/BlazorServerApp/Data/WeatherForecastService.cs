1e396992b676b425f3741c6a126006502b4f697b
aren <code@arencore.me>
2022-01-03 21:53:41 +0800

    component life scope

--- src/DotNetCore/BlazorServerApp/Data/WeatherForecastService.cs
+++ src/DotNetCore/BlazorServerApp/Data/WeatherForecastService.cs
@@ -1,7 +1,15 @@
+using Microsoft.Extensions.Caching.Memory;
+
 namespace BlazorServerApp.Data;
 
 public class WeatherForecastService
 {
+    private readonly IMemoryCache _memoryCache;
+    public WeatherForecastService(IMemoryCache memoryCache)
+    {
+        _memoryCache = memoryCache;
+    }
+
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -9,11 +17,19 @@ public class WeatherForecastService
 
     public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
     {
-        return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        return _memoryCache.GetOrCreateAsync(startDate, async e =>
         {
-            Date = startDate.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        }).ToArray());
+            e.SetOptions(new MemoryCacheEntryOptions { AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(30) });
+            var rng = new Random();
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            return (Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).ToArray());
+
+        });
+
     }
 }
