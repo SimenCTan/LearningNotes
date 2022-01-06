14616fb217a30195081969d26ab1548a921a5b9a
aren <code@arencore.me>
2022-01-06 19:03:37 +0800

    Add edit form validate

---
+++ src/DotNetCore/BlazorServerApp/Models/ComponentEnums.cs
@@ -0,0 +1,6 @@
+public class ComponentEnums
+{
+    public enum Manufacturer { SpaceX, NASA, ULA, VirginGalactic, Unknown }
+    public enum Color { ImperialRed, SpacecruiserGreen, StarshipBlue, VoyagerOrange }
+    public enum Engine { Ion, Plasma, Fusion, Warp }
+}
