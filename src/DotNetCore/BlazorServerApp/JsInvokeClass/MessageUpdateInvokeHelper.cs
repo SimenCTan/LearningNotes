bc69ab4c6f90c0004aec13638340b4511441e21e
aren <code@arencore.me>
2022-01-09 23:05:29 +0800

    Add js call .NET method

---
+++ src/DotNetCore/BlazorServerApp/JsInvokeClass/MessageUpdateInvokeHelper.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.JSInterop;
+
+public class MessageUpdateInvokeHelper
+{
+    private Action action;
+
+    public MessageUpdateInvokeHelper(Action action)
+    {
+        this.action = action;
+    }
+
+    [JSInvokable]
+    public void UpdateMessageCaller()
+    {
+        action.Invoke();
+    }
+
+}
