432c67731aae14a82c653ef3184d2ac21afe9d02
aren <code@arencore.me>
2022-03-18 20:27:10 +0800

    component parameter

---
+++ src/DotNetCore/BlazorNativeFile/Pages/CounterPartialClass.razor.cs
@@ -0,0 +1,10 @@
+namespace BlazorNativeFile.Pages;
+public partial class CounterPartialClass
+{
+    private int currentCount = 0;
+
+    private void IncrementCount()
+    {
+        currentCount++;
+    }
+}
