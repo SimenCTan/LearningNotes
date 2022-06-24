b9afbe5f99328c976a4ac4d7f27df54a41941629
aren <code@arencore.me>
2022-06-24 23:42:18 +0800

    add timezone test

---
+++ src/DotNetCore/TestTimezone/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(GetPublishDate(20220622, "21:02").ToString("yyyyMMdd HH:mm"));
+DateTime GetPublishDate(uint date, string hour)
+{
+    var publishDate = DateTime.ParseExact($"{date} {hour}", "yyyyMMdd HH:mm", CultureInfo.InvariantCulture);
+    var zeroTimezone = new DateTimeOffset(publishDate, TimeSpan.FromHours(0));
+    return zeroTimezone.ToUniversalTime().UtcDateTime;
+}
