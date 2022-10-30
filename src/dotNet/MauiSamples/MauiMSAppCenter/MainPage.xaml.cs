36c2cc148aaf9932eefc63493e91fbcb4a90919a
aren <code@arencore.me>
2022-10-30 21:51:42 +0800

    add maui appcenter

---
+++ src/dotNet/MauiSamples/MauiMSAppCenter/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+namespace MauiMSAppCenter;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+        Analytics.TrackEvent("count", new Dictionary<string, string> { { "count", count.ToString() } });
+        //Crashes.GenerateTestCrash();
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
---
+++ src/dotNet/MauiSamples/MauiMSAppCenter/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+namespace MauiMSAppCenter;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+        AppCenter.Start("windowsdesktop=f05a7743-44e7-40f7-89e3-981ac5a1a46f;" +
+                "android={Your Android App secret here};" +
+                "ios={Your iOS App secret here};" +
+                "macos={Your macOS App secret here};",
+                typeof(Analytics), typeof(Crashes));
+
+        return builder.Build();
+	}
+}
