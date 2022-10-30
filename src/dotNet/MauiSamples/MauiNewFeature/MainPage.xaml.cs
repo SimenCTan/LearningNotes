0e08a83c8176285f84555e9987fc3305682c4283
aren <code@arencore.me>
2022-10-30 22:25:07 +0800

    image capture

--- src/dotNet/MauiSamples/MauiNewFeature/MainPage.xaml.cs
+++ src/dotNet/MauiSamples/MauiNewFeature/MainPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using CommunityToolkit.Maui.Views;
 using MauiNewFeature.Views;
+using System.IO;
+
 namespace MauiNewFeature;
 
 public partial class MainPage : ContentPage
@@ -24,9 +26,15 @@ public partial class MainPage : ContentPage
         //myImage.Source = ImageSource.FromStream(() => stream);
 
         // popup
-        var popup = new PopupSimple();
-        popup.Anchor = CounterBtn;
-        this.ShowPopup(popup);
+        //var popup = new PopupSimple();
+        //popup.Anchor = CounterBtn;
+        //this.ShowPopup(popup);
+
+        // capture image
+        var result = await screenCapture.CaptureAsync();
+        using MemoryStream stream = new();
+        await result.CopyToAsync(stream, ScreenshotFormat.Png);
+        await File.WriteAllBytesAsync("D:\\github\\LearningNotes\\src\\dotNet\\MauiSamples\\MauiNewFeature\\images\\mauiscreen.png",stream.ToArray());
     }
 }
 
