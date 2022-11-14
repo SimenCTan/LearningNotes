b710d77821d11df003ac38e88da6006c4dc0daea
aren <code@arencore.me>
2022-11-14 19:48:24 +0800

    :arrow_up: maui net7

---
+++ src/dotNet/MauiSamples/MauiAppNET7/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiAppNET7;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ src/dotNet/MauiSamples/MauiAppNET7/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiAppNET7;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
