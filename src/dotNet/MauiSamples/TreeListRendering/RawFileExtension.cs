fdfec653efa97d889ce345f7054e5147a5137d34
aren <code@arencore.me>
2023-11-29 18:05:04 +0800

    add json data

---
+++ src/dotNet/MauiSamples/TreeListRendering/RawFileExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace TreeListRendering;
+
+public static class RawFileExtension
+{
+    public async static Task<string> ReadAccountsDataAsync()
+    {
+        using var stream = await FileSystem.OpenAppPackageFileAsync("accounts_data.json");
+        using (var reader = new StreamReader(stream))
+        {
+            return reader.ReadToEnd();
+        }
+    }
+}
