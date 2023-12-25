ad187513bdb44076911efdfa86b4776f55c9c5b9
aren <code@arencore.me>
2023-12-25 15:56:27 +0800

    day05

--- src/JavaScript/react/30days-of-react/05/prop-react-app/src/index.js
+++ src/JavaScript/react/30days-of-react/05/prop-react-app/src/index.js
@@ -1,143 +1,152 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import asabenehImage from "./images/asabeneh.jpg";
 
-const getUserInfo = (firstName, lastName, country) => {
-  return `${firstName} ${lastName}. Lives in ${country}.`;
-};
+// Fuction to show month date year
 
-// calling a functons
-getUserInfo("Asabeneh", "Yeteyeh", "Finland");
-
-
-// Function to display time in Mon date, year format eg Oct 4, 2020
 const showDate = (time) => {
   const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ]
 
-  const month = months[time.getMonth()].slice(0, 3);
-  const year = time.getFullYear();
-  const date = time.getDate();
-  return ` ${month} ${date}, ${year}`;
-};
+  const month = months[time.getMonth()].slice(0, 3)
+  const year = time.getFullYear()
+  const date = time.getDate()
+  return ` ${month} ${date}, ${year}`
+}
 
-
-// simple component
-const ClickButton = (props) => (
-  <button onClick={props.onClick}>{props.text}</button>
-);
-
-// User component, component should start with an uppercase
-const User = (props) => {
-  return (
-    <div>
-      <h1>
-        {props.firstName}
-        {props.lastName}
-      </h1>
-      <small>{props.country}</small>
-    </div>
-  );
-};
-const Age = (props) => <div>The person is {props.age} years old.</div>;
-const Weight = (props) => (
-  <p>The weight of the object on earth is {props.weight} N.</p>
-);
-const Status = (props) => {
-  // ternary operator to check the status of the person
-  let status = props.status ? "Old enough to drive" : "Too young for driving";
-  return <p>{status}</p>;
-};
-// const Skills = (props) =>{
-//   const skillsList = props.skills.map((skill)=> <li>{skill}</li>);
-//   return <ul>skillsList</ul>;
-// }
-
-// Skills Component
-const Skills = (props) => {
-  // modifying the skills array
-  const skillList = props.skills.map((skill) => <li>{skill}</li>);
-  return <ul>{skillList}</ul>;
-};
 // Header Component
-// JSX element, header
-// const welcome = 'Welcome to 30 Days Of React'
-// const title = 'Getting Started React'
-// const subtitle = 'JavaScript Library'
-// const author = {
-//   firstName: 'Asabeneh',
-//   lastName: 'Yetayeh',
-// }
-// const date = 'Oct 2, 2020'
-const Header = (props) => {
-  console.log(props);
+const Header = ({
+  data: {
+    welcome,
+    title,
+    subtitle,
+    author: { firstName, lastName },
+    date,
+  },
+}) => {
   return (
     <header>
-      <div className="header-wrapper">
-        <h1>{props.welcome}</h1>
-        <h2>{props.title}</h2>
-        <h3>{props.subtitle}</h3>
+      <div className='header-wrapper'>
+        <h1>{welcome}</h1>
+        <h2>{title}</h2>
+        <h3>{subtitle}</h3>
         <p>
-          {props.firstName} {props.lastName}
+          {firstName} {lastName}
         </p>
-        <small>{props.date}</small>
+        <small>{showDate(date)}</small>
       </div>
     </header>
-  );
-};
+  )
+}
+
+// TechList Component
+const TechList = ({ techs }) => {
+  const techList = techs.map((tech) => <li key={tech}>{tech}</li>)
+  return techList
+}
+
+// User Card Component
+const UserCard = ({ user: { firstName, lastName, image } }) => (
+  <div className='user-card'>
+    <img src={image} alt={firstName} />
+    <h2>
+      {firstName}
+      {lastName}
+    </h2>
+  </div>
+)
+
+// A button component
+
+const Button = ({ text, onClick, style }) => (
+  <button style={style} onClick={onClick}>
+    {text}
+  </button>
+)
+
+// CSS styles in JavaScript Object
+const buttonStyles = {
+  backgroundColor: '#61dbfb',
+  padding: 10,
+  border: 'none',
+  borderRadius: 5,
+  margin: 3,
+  cursor: 'pointer',
+  fontSize: 18,
+  color: 'white',
+}
+
+// Main Component
+const Main = ({ user, techs, greetPeople, handleTime }) => (
+  <main>
+    <div className='main-wrapper'>
+      <p>Prerequisite to get started react.js:</p>
+      <ul>
+        <TechList techs={techs} />
+      </ul>
+      <UserCard user={user} />
+      <Button text='Greet People' onClick={greetPeople} style={buttonStyles} />
+      <Button text='Show Time' onClick={handleTime} style={buttonStyles} />
+    </div>
+  </main>
+)
+
+// Footer Component
+const Footer = ({ copyRight }) => (
+  <footer>
+    <div className='footer-wrapper'>
+      <p>Copyright {copyRight.getFullYear()}</p>
+    </div>
+  </footer>
+)
 
 const App = () => {
-  const welcome = "Welcome to 30 Days Of React";
-  const title = "Getting Started React";
-  const subtitle = "JavaScript Library";
-  const firstName = "Asabeneh";
-  const lastName = "Yetayeh";
-  const date = "Oct 4, 2020";
-  let currentYear = 2020;
-  let birthYear = 1820;
-  const age = currentYear - birthYear;
-  const gravity = 9.81;
-  const mass = 75;
-  const skills = ["HTML", "CSS", "JavaScript"];
-  let status = age >= 18;
-  const sayHi=()=>{
-    alert('Hi');
-  };
- const handleTime = () => {
-   alert(showDate(new Date()));
- };
- const greetPeople = () => {
-   alert("Welcome to 30 Days Of React Challenge, 2020");
- };
-  return (
-    <div className="app">
-      <Header
-        welcome={welcome}
-        title={title}
-        subtitle={subtitle}
-        firstName={firstName}
-        lastName={lastName}
-        date={date}
-      />
-      <Age age={age} />
-      <Weight weight={gravity * mass} />
-      <Status status={status} />
-      <Skills skills={skills} />
-      <ClickButton text="show time" onClick={handleTime} />
-      <ClickButton text="Greet People" onClick={greetPeople} />
-    </div>
-  );
+   const data = {
+     welcome: "Welcome to 30 Days Of React",
+     title: "Getting Started React",
+     subtitle: "JavaScript Library",
+     author: {
+       firstName: "Asabeneh",
+       lastName: "Yetayeh",
+     },
+     date: new Date(), // date needs to be formatted to a human readable format
+   };
+   const date = new Date();
+   const techs = ["HTML", "CSS", "JavaScript"];
+   // copying the author from data object to user variable using spread operator
+   const user = { ...data.author, image: asabenehImage };
+
+   const handleTime = () => {
+     alert(showDate(new Date()));
+   };
+   const greetPeople = () => {
+     alert("Welcome to 30 Days Of React Challenge, 2020");
+   };
+
+   return (
+     <div className="app">
+       <Header data={data} />
+       <Main
+         user={user}
+         techs={techs}
+         handleTime={handleTime}
+         greetPeople={greetPeople}
+       />
+       <Footer copyRight={date} />
+     </div>
+   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
