f883a9d2b0dc314ae1f1d30d1239d077794b06f2
aren <code@arencore.me>
2023-12-25 23:56:47 +0800

    class component

--- src/JavaScript/react/30days-of-react/07/class-react-app/src/index.js
+++ src/JavaScript/react/30days-of-react/07/class-react-app/src/index.js
@@ -2,24 +2,54 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import asabenehImage from "./images/asabeneh.jpg";
 
+// A button component
+const Button = ({ text, onClick, style }) => (
+  <button style={style} onClick={onClick}>
+    {text}
+  </button>
+);
 
+// CSS styles in JavaScript Object
+const buttonStyles = {
+  backgroundColor: "#61dbfb",
+  padding: 10,
+  border: "none",
+  borderRadius: 5,
+  margin: 3,
+  cursor: "pointer",
+  fontSize: 18,
+  color: "white",
+};
 
+// User Card Component
+const UserCard = ({ user: { firstName, lastName, image } }) => (
+  <div className='user-card'>
+    <img src={image} alt={firstName} />
+    <h2>
+      {firstName}
+      {lastName}
+    </h2>
+  </div>
+)
 
 // class base component
 class Header extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     // the code inside the constructor run before any other code
   }
+  greetPeople = () => {
+    alert("Welcome to 30 Days Of React Challenge, 2020");
+  };
   render() {
-     console.log(this.props.data);
-     const {
-       welcome,
-       title,
-       subtitle,
-       author: { firstName, lastName },
-       date,
-     } = this.props.data;
+    console.log(this.props.data);
+    const {
+      welcome,
+      title,
+      subtitle,
+      author: { firstName, lastName },
+      date,
+    } = this.props.data;
     return (
       <header>
         <div className="header-wrapper">
@@ -30,6 +60,7 @@ class Header extends React.Component {
             {firstName} {lastName}
           </p>
           <small>{date}</small>
+          <button onClick={this.greetPeople}> Greet </button>
         </div>
       </header>
     );
@@ -37,13 +68,13 @@ class Header extends React.Component {
 }
 
 class TechList extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
   }
-  render(){
-      const { techs } = this.props;
-      const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>);
-      return techsFormatted;
+  render() {
+    const { techs } = this.props;
+    const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>);
+    return techsFormatted;
   }
 }
 
@@ -51,7 +82,7 @@ class TechList extends React.Component {
 // Class Component
 class Main extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
   render() {
     return (
@@ -61,18 +92,28 @@ class Main extends React.Component {
           <ul>
             <TechList techs={this.props.techs} />
           </ul>
+          <UserCard user={this.props.user} />
+          <Button
+            text="Greet People"
+            onClick={this.props.greetPeople}
+            style={buttonStyles}
+          />
+          <Button
+            text="Show Time"
+            onClick={this.props.handleTime}
+            style={buttonStyles}
+          />
         </div>
       </main>
     );
   }
 }
 
-
 // Footer Component
 // Class component
 class Footer extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
   render() {
     return (
@@ -89,8 +130,37 @@ class Footer extends React.Component {
 // Class Component
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
+
+  showDate = (time) => {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    const month = months[time.getMonth()].slice(0, 3);
+    const year = time.getFullYear();
+    const date = time.getDate();
+    return ` ${month} ${date}, ${year}`;
+  };
+
+  handleTime = () => {
+    alert(this.showDate(new Date()));
+  };
+  greetPeople = () => {
+    alert("Welcome to 30 Days Of React Challenge, 2020");
+  };
   render() {
     const data = {
       welcome: "Welcome to 30 Days Of React",
@@ -103,10 +173,14 @@ class App extends React.Component {
       date: "Oct 7, 2020",
     };
     const techs = ["HTML", "CSS", "JavaScript"];
+
+    // copying the author from data object to user variable using spread operator
+    const user = { ...data.author, image: asabenehImage };
+
     return (
       <div className="app">
         <Header data={data} />
-        <Main techs={techs} />
+        <Main user={user} techs={techs} handleTime={this.handleTime} greetPeople={this.greetPeople} />
         <Footer date={new Date()} />
       </div>
     );
