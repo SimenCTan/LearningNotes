216adcae43c4e558ed9f107519ec30ec77b55369
aren <code@arencore.me>
2023-12-28 13:04:25 +0800

    add day 17

---
+++ src/JavaScript/react/30days-of-react/17/README.md
@@ -0,0 +1,14 @@
+# React Router
+You may have not heard of the word route or router before and it might be necessary to define it here. The literal meaning of route is a path or a way to get to somewhere. The meaning in React is also similar to the literal meaning. React Router is by itself a React component which allows you to navigate between React components.
+
+## BroswerRouter
+BrowerRouter is a parent component which allows to wrap the application route. Using the BrowserRouter we can access the browser history. Sometimes it can renames as router.
+
+## Route
+The Route component allows to navigate between components. It is a pathway from one component to another. The Route component has two required props: the path and component or render.
+
+## Nested Routing
+We have implemented a simple navigation using React Router. Now, let's see how we can also nest a route. It possible to have a nested route in React.
+
+## Redirect
+Redirect can help us to redirect a route to a certain path based some condition. For instance if a user is logged in we redirect it to the dashboard otherwise to the login page. Let's implement a fake login in above snippet of code. If a user logged in it will redirected to the challenges otherwise we suggest the user to login.
--- src/JavaScript/react/30days-of-react/17/router-react-app/src/index.js
+++ src/JavaScript/react/30days-of-react/17/router-react-app/src/index.js
@@ -1,20 +1,61 @@
-// index.js
-import React,{Component} from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter,Route,NavLink,Switch,Redirect,Prompt,withRouter} from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Routes,
+} from "react-router-dom";
+
+// Home component
+const Home = (props) => <h1>Welcome Home</h1>;
+// About component
+const About = (props) => <h1>About Us</h1>;
+// Contact component
+const Contact = (props) => <h1>Contact us</h1>;
+// Challenge component
+const Challenges = (props) => (
+  <div>
+    <h1>30 Days Of React Challenge</h1>
+  </div>
+);
+
+const NotFound = (props) => <h1>The page your looking for not found</h1>;
+const Navbar = () => (
+  <ul>
+    <li>
+      <NavLink to="/">Home</NavLink>
+    </li>
+    <li>
+      <NavLink to="/about">About</NavLink>
+    </li>
+    <li>
+      <NavLink to="/contact">Contact</NavLink>
+    </li>
+    <li>
+      <NavLink to="/challenges">Challenges</NavLink>
+    </li>
+  </ul>
+);
 
 class App extends Component {
-  render(){
+  render() {
     return (
-      <BrowserRouter>
+      <Router>
         <div className="App">
-          <h1>React Router DOM</h1>
+          <Navbar/>
+          <Routes>
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/challenges" component={Challenges} />
+            <Route path="/" component={Home} />
+            <Route component={NotFound} />
+          </Routes>
         </div>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
-
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
