7b8b90757118da4a7de61499181c4c1437f514ba
aren <code@arencore.me>
2023-12-29 00:00:22 +0800

    add vite

--- src/JavaScript/react/30days-of-react/17/vite-react-app/src/routes/contact.jsx
+++ src/JavaScript/react/30days-of-react/17/vite-react-app/src/routes/contact.jsx
@@ -1,9 +1,21 @@
-import { Form,useLoaderData } from "react-router-dom";
-import { getContact } from "../contact";
+import { Form, useLoaderData, useFetcher } from "react-router-dom";
+import { getContact, updateContact } from "../contact";
 
+export async function action({ request, params }) {
+  let formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  });
+}
 
-export async function loader({params}){
+export async function loader({ params }) {
   const contact = await getContact(params.contactId);
+  if(!contact){
+    throw new Response("",{
+      status:404,
+      statusText:"Not Found"
+    });
+  }
   return { contact };
 }
 
@@ -38,7 +50,11 @@ export default function Contact() {
 
         {contact.twitter && (
           <p>
-            <a target="_blank" href={`https://twitter.com/${contact.twitter}`} rel="noreferrer">
+            <a
+              target="_blank"
+              href={`https://twitter.com/${contact.twitter}`}
+              rel="noreferrer"
+            >
               {contact.twitter}
             </a>
           </p>
@@ -70,8 +86,12 @@ export default function Contact() {
 function Favorite({ contact }) {
   // yes, this is a `let` for later
   let favorite = contact.favorite;
+  const fetcher = useFetcher();
+  if (fetcher.formData) {
+     favorite = fetcher.formData.get("favorite") === "true";
+   }
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         name="favorite"
         value={favorite ? "false" : "true"}
@@ -79,6 +99,6 @@ function Favorite({ contact }) {
       >
         {favorite ? "★" : "☆"}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 }
--- src/JavaScript/react/30days-of-react/17/vite-react-app/src/routes/root.jsx
+++ src/JavaScript/react/30days-of-react/17/vite-react-app/src/routes/root.jsx
@@ -1,3 +1,4 @@
+import {useEffect} from 'react'
 import {
   Outlet,
   useLoaderData,
@@ -5,12 +6,15 @@ import {
   redirect,
   NavLink,
   useNavigation,
+  useSubmit
 } from "react-router-dom";
 import { getContacts, createContact } from "../contact";
 
-export async function loader() {
-  const contacts = await getContacts();
-  return { contacts };
+export async function loader({request}) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get('q');
+  const contacts = await getContacts(q);
+  return { contacts,q };
 }
 
 export async function action() {
@@ -19,24 +23,36 @@ export async function action() {
 }
 
 export default function Root() {
-  const { contacts } = useLoaderData();
+  const { contacts,q } = useLoaderData();
   const navigation = useNavigation();
+  useEffect(()=>{
+    document.getElementById('q').value=q;
+  },[q]);
+  const submit = useSubmit();
+  const searching = navigation.location && new URLSearchParams(navigation.location.search).has('q');
+
   return (
     <>
       <div id="sidebar">
         <h1>React Router Contacts</h1>
         <div>
-          <form id="search-form" role="search">
+          <Form id="search-form" role="search">
             <input
               id="q"
+              className={searching?"loading":""}
               aria-label="Search contacts"
               placeholder="Search"
               type="search"
               name="q"
+              defaultValue={q}
+              onChange={(e)=>{
+                const isFirstSearch = q==null;
+                submit(e.currentTarget.form,{replace:!isFirstSearch});
+              }}
             />
-            <div id="search-spinner" aria-hidden hidden={true} />
+            <div id="search-spinner" aria-hidden hidden={!searching} />
             <div className="sr-only" aria-live="polite"></div>
-          </form>
+          </Form>
           <Form method="post">
             <button type="submit">New</button>
           </Form>
