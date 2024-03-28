129835b3b9cbd997cef99233860fb087a600c294
arencode <code@arencore.me>
2024-03-28 19:42:58 +0800

    add doc

---
+++ src/dotNet/BlogSamples/ReaderDoc/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using DocumentFormat.OpenXml;
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// Replace with the actual path to your .doc file
+string docFilePath = "./data/TestMickey.doc";
+
+// Create a new .docx file
+string docxFilePath = Path.ChangeExtension(docFilePath, ".docx");
+
+using WordprocessingDocument docx = WordprocessingDocument.Create(docxFilePath, WordprocessingDocumentType.Document);
+MainDocumentPart mainPart = docx.AddMainDocumentPart();
+mainPart.Document = new Document(new Body());
+
+// Read the content from the .doc file
+string docContent = File.ReadAllText(docFilePath);
+
+Console.WriteLine(docContent);
