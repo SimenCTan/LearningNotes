6359e980f50267de52cf28ce16a833174e41d150
aren <code@arencore.me>
2024-07-30 15:15:05 +0800

    use pnpm

--- src/JavaScript/vue/vue3-uniapp-project/src/App.vue
+++ src/JavaScript/vue/vue3-uniapp-project/src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
 onLaunch(() => {
-  console.log("App Launch");
+  console.log("App Launch test");
 });
 onShow(() => {
-  console.log("App Show");
+  console.log("App Show ");
 });
 onHide(() => {
   console.log("App Hide");
