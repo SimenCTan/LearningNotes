013e1afc3aeba20d41071c48a0cb9c3fd0a594d4
aren <code@arencore.me>
2024-07-30 17:20:01 +0800

    add uniapp static image

--- src/JavaScript/vue/vue3-uniapp-project/src/pages/index/index.vue
+++ src/JavaScript/vue/vue3-uniapp-project/src/pages/index/index.vue
@@ -4,12 +4,44 @@
     <view class="text-area">
       <text class="title">{{ title }}</text>
     </view>
+    <view class="pictures">
+      <image
+        v-for="picture in pictures"
+        :key="picture.id"
+        :src="picture.url"
+        @click="onPreviewImage(picture.url)"
+        class="picture"
+      />
+    </view>
   </view>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
+
 const title = ref("Hello uni-app test");
+const pictures = ref([
+  {
+    id: "1",
+    url: "/static/images/goods_preview_1.jpg",
+  },
+  {
+    id: "2",
+    url: "/static/images/goods_preview_2.jpg",
+  },
+  {
+    id: "3",
+    url: "/static/images/goods_preview_3.jpg",
+  },
+]);
+
+const onPreviewImage = (url: string) => {
+  console.log("onPreviewImage", url);
+  uni.previewImage({
+    urls: pictures.value.map((item) => item.url),
+    current: url,
+  });
+};
 </script>
 
 <style>
@@ -19,23 +51,15 @@ const title = ref("Hello uni-app test");
   align-items: center;
   justify-content: center;
 }
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
+.pictures {
   display: flex;
-  justify-content: center;
+  flex-wrap: wrap;
+  gap: 10px;
 }
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+.picture {
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+  cursor: pointer;
 }
 </style>
