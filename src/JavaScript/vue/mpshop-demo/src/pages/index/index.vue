e8217795d93136a1b8ec83f80aac58f5f731d1e4
aren <code@arencore.me>
2024-07-30 23:24:06 +0800

    add uni ui

--- src/JavaScript/vue/mpshop-demo/src/pages/index/index.vue
+++ src/JavaScript/vue/mpshop-demo/src/pages/index/index.vue
@@ -3,7 +3,14 @@
 </script>
 
 <template>
-  <view class="index">indextest</view>
+  <uni-card
+    title="基础卡片test"
+    sub-title="副标题"
+    extra="额外信息"
+    thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+  >
+    <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
+  </uni-card>
 </template>
 
 <style lang="scss">
