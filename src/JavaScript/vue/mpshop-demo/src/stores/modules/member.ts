8ee8290cc9c3c595b04509d73fb9a6fd48185ace
aren <code@arencore.me>
2024-07-31 11:06:23 +0800

    add pinia

--- src/JavaScript/vue/mpshop-demo/src/stores/modules/member.ts
+++ src/JavaScript/vue/mpshop-demo/src/stores/modules/member.ts
@@ -27,6 +27,11 @@ export const useMemberStore = defineStore(
   },
   // TODO: 持久化
   {
-    persist: true,
+    persist: {
+      storage: {
+        getItem: (key: string) => uni.getStorageSync(key),
+        setItem: (key: string, value: string) => uni.setStorageSync(key, value),
+      },
+    },
   },
 )
